534137cc9e236279e91d91ffcdbfc39f12a0196b
CurtisFenner <cfenneremail@gmail.com>
2014-10-13 14:48:50 -0400

    Player Keyboard Simulteanous Up/Down Left/Right

--- site/js/logic/player.js
+++ site/js/logic/player.js
@@ -4,15 +4,15 @@ function playerThink(player) {
 	player.wx = 0;
 	player.wy = 0;
 	if (Keyboard.LEFT)  {
-		player.wx = -1;
+		player.wx += -1;
 	}
 	if (Keyboard.RIGHT) {
-		player.wx = 1;
+		player.wx += 1;
 	}
 	if (Keyboard.UP)    {
-		player.wy = -1;
+		player.wy += -1;
 	}
 	if (Keyboard.DOWN)  {
-		player.wy = 1;
+		player.wy += 1;
 	}
 }
