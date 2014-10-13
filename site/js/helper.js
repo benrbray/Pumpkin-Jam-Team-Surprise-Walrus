e7a7549ecc6a3361dc52d0236d971e9984f97a9f
Benjamin Bray <benrbray@umich.edu>
2014-10-13 13:09:49 -0400

    helper.js comments

--- site/js/helper.js
+++ site/js/helper.js
@@ -2,20 +2,12 @@ function sign(x) {
 	return x?x<0?-1:1:0;
 }
 
-
+// converts RGBA to HTML color string
 function htmlColor(r, g, b, a) {
-	/*
-	 * Why not use
-	r &= 0xff;
-	g &= 0xff;
-	b &= 0xff;
-	 * to ensure that rgba is between 0 and 255?
-	 * ~Tyler
-	 */
-
-	if (typeof a != typeof 1) {
-		a = 1;
-	}
+	// ensure alpha is a number
+	if (typeof a != typeof 1) { a = 1; }
+	
+	// generate color string
 	return ( "rgba("
 		+ (r&0xff) +  ","
 		+ (g&0xff) + "," 
