3f3b3ca190b1d75ab5a04daf20d7d29da2e31a24
Benjamin Bray <benrbray@umich.edu>
2014-10-13 13:15:16 -0400

    added camera.js

---
+++ site/js/camera.js
@@ -0,0 +1,17 @@
+/* CAMERA.JS
+ * Contains camera information and effects (zoom, shake, etc.).
+ */
+
+"use strict"
+
+////////////////////////////////////////////////////////////////////////////////
+
+var Camera = {
+	x:	0,	// tiles
+	y:	0,	// tiles
+	tilesHorizontal:	20
+}
+
+Camera.contains = function(x, y){
+	// TODO
+}
