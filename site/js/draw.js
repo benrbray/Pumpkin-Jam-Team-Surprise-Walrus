d99af513db7c2f6710dd77506e4665c775a09d2b
CurtisFenner <cfenneremail@gmail.com>
2014-10-13 18:57:21 -0400

    Cursor Update

--- site/js/draw.js
+++ site/js/draw.js
@@ -24,4 +24,11 @@ function draw() {
 	
 	// Reset Canvas Coordinates for drawing GUI
 	context.setTransform(1,0,0,1,0,0);
+
+	context.lineWidth = 1;
+	context.strokeStyle = "rgba(0,0,0,0.5)";
+	context.strokeCircle(Mouse.x,Mouse.y,2);
+	context.strokeCircle(Mouse.x,Mouse.y,4);
+	context.strokeStyle = "rgba(255,255,255,0.5)";
+	context.strokeCircle(Mouse.x,Mouse.y,3);
 }
