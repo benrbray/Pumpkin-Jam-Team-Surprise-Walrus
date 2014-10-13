798eac678e657111bff79cbc320471138a5f9f9a
Benjamin Bray <benrbray@umich.edu>
2014-10-13 16:58:38 -0400

    separated Player GameAsset into new file

--- site/js/assets/GameAsset.js
+++ site/js/assets/GameAsset.js
@@ -58,35 +58,6 @@ function GameAsset(width, height, walkGrid, drawFunction){
 // Natural Elements
 GameAsset.treeTrunk = new GameAsset(1,1, false, "wal.png");
 
-// Player
-GameAsset.player = new GameAsset(0.8, 0.8, true, function(x,y,asset,obj) {
-	context.save();
-	context.translate(x,y);
-	if (obj.vx * obj.vx + obj.vy * obj.vy > 0.03 * 0.03) {
-		obj.drawx = obj.drawx * 0.9 + 0.1 * obj.vx;
-		obj.drawy = obj.drawy * 0.9 + 0.1 * obj.vy;
-		if (!isFinite(obj.drawx)) {
-			obj.drawx = 0;
-			obj.drawy = 0;
-		}
-	}
-	context.rotate( Math.atan2(  obj.drawy, obj.drawx ) + Math.PI / 2 );
-	if (obj.leapwait > 45) {
-		// Motion blur
-		for (var i = 0; i < 1.2; i += 0.4) {
-			context.globalAlpha = (1 - i) / 1.5;
-			context.drawImage(asset.img,-.25,-.75 + i, 0.5, 1.5);
-		}
-		context.globalAlpha = 1;
-	}
-	context.drawImage(asset.img,-.25,-.75, 0.5, 1.5);
-	context.restore();
-});
-GameAsset.player.img = new Image();
-GameAsset.player.img.src = "assets/graphics/wolf_reference.png";
-
-// Humans
-
 // Settlements
 GameAsset.cabin = new GameAsset(4, 3, false);
 
