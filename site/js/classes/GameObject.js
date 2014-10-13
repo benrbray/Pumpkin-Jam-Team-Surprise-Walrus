eb5d099e6b8a2612a9aedd76573fb17fe421f64a
Benjamin Bray <benrbray@umich.edu>
2014-10-13 00:01:36 -0400

    created GameObject

---
+++ site/js/classes/GameObject.js
@@ -0,0 +1,13 @@
+/* GAMEOBJECT.JS
+ * Handles dynamic objects not necessarily aligned to grid cells.
+ */
+
+"use strict";
+
+////////////////////////////////////////////////////////////////////////////////
+
+function GameObject(x, y, gameAsset){
+	this.xTile = xTile;
+	this.yTile = yTile;
+	this.gameAsset = gameAsset;
+}
