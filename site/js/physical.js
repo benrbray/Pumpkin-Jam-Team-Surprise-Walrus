df44b3984797947f93cdc9a5d43996118e3faa8e
CurtisFenner <cfenneremail@gmail.com>
2014-10-12 23:38:50 -0400

    Potential Physics Solution?
    I'm not sure whether or not this is sufficient for nice controls.

--- site/js/physical.js
+++ site/js/physical.js
@@ -27,18 +27,24 @@ World.hasSpace = function(x,y,r) {
 // (in this frame) (vx,vy) 
 // The box has "radius" r (dimensions 2r x 2r)
 // ||(vx, vy)|| must be less than 1 for in general correct behavior
+// Returns [newx, newy, % velocity horizontal used, % velocity vertical used]
 World.move = function(x,y , r, vx,vy) {
 	var canMove = World.hasSpace(x + vx,y + vy,r);
-
 	if (canMove) {
-		return [x + vx, y + vy];
+		return [x + vx, y + vy, 1, 1];
 	} else {
 		var m = Math.sqrt(vx * vx + vy * vy);
 		for (var j = m; j > 0; j -= 0.5) {
 			if (World.hasSpace(x + vx * j / m,y + vy * j / m)) {
-				return [x + vx * j / m,y + vy * j / m];
+				var larger = Math.max(vx*vx,vy*vy);
+				var largerHorizontal = vx*vx == larger;
+				return [
+					x + vx * j / m,y + vy * j / m, 
+					largerHorizontal ? 0 : 1, 
+					!largerHorizontal ? 0 : 1
+				];
 			}
 		}
-		return [x,y];
+		return [x,y, 0,0];
 	}
 }
