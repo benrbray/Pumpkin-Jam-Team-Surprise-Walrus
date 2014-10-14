7ee11252c9cb092633c3a8771b76924947c618d8
Tyler Dence <eggnog5.1@gmail.com>
2014-10-13 22:47:07 -0500

    Change audio system

--- site/js/audio.js
+++ site/js/audio.js
@@ -1,6 +1,40 @@
 var Sounds = {};
-Sounds.musicMeadow = new Audio("assets/audio/wandering.mp3");
-Sounds.musicMeadow.loop = true;
-Sounds.musicMeadow.volume = 0.1;
-Sounds.musicMeadow.play();
+function getAudioData(url, callback, audioContext) {
+	var request = new XMLHttpRequest();
+	request.open("GET", url, true);
+	request.responseType = "arraybuffer";
 
+	request.onload = function() {
+		audioContext.decodeAudioData(request.response, function(data) {
+				callback(data, undefined);
+			},
+			function() {
+				console.log("Error decoding file '" + url + "'" );
+			});
+	}
+	request.send();
+}
+
+Sounds.sound = function(url){
+	this.url = url;
+	this.buffer = null;
+	this.audioContext = new AudioContext();
+
+	this.play = function(loop){
+		var obj = this;
+		getAudioData(this.url, function(data) {
+			obj.source = obj.audioContext.createBufferSource();
+			obj.source.buffer = data;
+			obj.source.loop = loop;
+			obj.source.connect(obj.audioContext.destination);
+			obj.source.start(0);
+		}, this.audioContext);
+	}
+
+	this.pause = function() {
+		this.source.stop(0);
+	}
+}
+
+Sounds.musicMeadow = new Sounds.sound("/assets/audio/wandering.mp3");
+Sounds.musicMeadow.play(true);
