2ef4126922c7ac9f0e8cb0c2e3710e9b9bbab1dc
Benjamin Bray <benrbray@umich.edu>
2014-10-14 13:23:03 -0400

    wolf movement & vector helpers

--- site/js/Vector.js
+++ site/js/Vector.js
@@ -6,8 +6,8 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 function Vector(x,y){
-	this.x = x;
-	this.y = y;
+	this.x = x || 0;
+	this.y = y || 0;
 }
 
 // Vector Operations -----------------------------------------------------------
@@ -36,7 +36,45 @@ Vector.prototype.from = function(v){
 
 // normal vector
 Vector.prototype.norm = function(){
+	if(this.mag()<0.0001){ console.log("TAKING NORM OF ZERO"); }
 	return this.multiply(this.mag());
+}
+
+// rotate this vector by theta radians
+Vector.prototype.rotate = function(radians){
+	var c = Math.cos(radians);
+	var s = Math.sin(radians);
+	return new Vector(this.x * c - this.y * s, this.x * s + this.y * c);
+}
+
+Vector.prototype.rotateTowards = function(v){
+	var dotprod = this.norm().dot(v.norm());
+	var t = 0.1 * Math.acos(dotprod);
+	
+	var u1 = this.rotate(t);
+	var u2 = this.rotate(-t);
+	var d1 = v.distanceTo(u1);
+	var d2 = v.distanceTo(u2);
+	var d3 = v.distanceTo(this);
+	
+	if (d3 < d1 && d3 < d2) {
+	return this;
+	}
+	
+	if(d1 < d2) return u1;
+	else return u2;
+	
+}
+
+Vector.prototype.rotateTowards2 = function(v, amount){
+	var angle = this.angleTo(v);
+	if(angle >  Math.PI) angle = angle - 2*Math.PI;
+	if(angle < -Math.PI) angle = 2*Math.PI + angle;
+	return this.rotate(angle*amount);
+}
+
+Vector.prototype.crossMag = function(v){
+	return this.x*v.y - this.y*v.x;
 }
 
 // Scalar Operations -----------------------------------------------------------
@@ -50,10 +88,21 @@ Vector.prototype.mag = function(){
 	return Math.sqrt(this.x*this.x + this.y*this.y);
 }
 // distance to another vector
-Vector.prototype.distTo = function(v){
+Vector.prototype.distanceTo = function(v){
 	return this.to(v).mag();
 }
 
 Vector.prototype.angle = function(){
-	return Math.atan2(this.y/this.x);
+	return Math.atan2(this.y, this.x);
+}
+
+Vector.prototype.angleTo = function(v){
+	/*var difference = v.angle() - this.angle();
+	difference = (difference + (Math.PI * 2)) % (Math.PI * 2);
+	if (difference > Math.PI) {
+		
+	} else {
+		
+	}*/
+	return v.angle() - this.angle();
 }
--- site/js/assets/player_asset.js
+++ site/js/assets/player_asset.js
@@ -5,39 +5,150 @@
 "use strict"
 
 ////////////////////////////////////////////////////////////////////////////////
-
+var tick = 0;
 // Player
 GameAsset.player = new GameAsset(0.8, 0.8, true, function(x,y,asset,obj) {
-
 	context.save();
-	// Save to leave untouched by this function
-
-
+	// Player Coordinates ------------------------------------------------------
+	
+	// Move origin to player center
 	context.translate(x,y);
-	// Move context origin to player's center
+	asset.vel.x = obj.vx;
+	asset.vel.y = obj.vy;
+	
+	// Move Trailing Segments --------------------------------------------------
+	
+	// Draw and position remaining segments
+	for(var i = 1; i < asset.segments.length; i++){
+		var follow = asset.segments[i-1];	// segment to follow
+		var segment = asset.segments[i];	// current segment to control
+		
+		// Move towards back end of 'follow'
+		var goalPos = follow.pos.subtract(follow.dir.norm().multiply(follow.size+follow.offset));
+		segment.pos = segment.pos.add( 
+			segment.pos.to( goalPos )
+		);
+		
+		// Rotate in direction of 'follow'
+		segment.dir = segment.dir.rotateTowards2(follow.dir, segment.spring);
+		
+		// Draw beginpath moveto lineto stroke
+		context.save();
+		context.fillStyle = "rgba(0,0,0,0.5)";
+		context.translate(segment.pos.x, segment.pos.y);
+		
+		var offset = 0;
+		if(i==3){
+			offset = Math.sin(tick*Math.sin(tick/200)/20)/3;
+			tick++;
+		}
+		
+		context.rotate(segment.dir.angle()+offset);
+		if(segment.img){
+			context.drawImage(
+				segment.img,					// image
+				-segment.size,	// top-left x
+				-segment.width/2, 				// top-left y
+				segment.size,					// length
+				segment.width					// thickness
+			);
+		}
+		context.restore();
+	}
+	
+	// Move Head ---------------------------------------------------------------
+	
+	// First Segment
+	var headSegment = asset.segments[0];
+	
+	// If player is moving, rotate head in direction of velocity
+	if(asset.vel.mag() > 0.03){
+		headSegment.dir = headSegment.dir.rotateTowards2(asset.vel, 1/10);
+	}
+	
+	// Draw first segment
+	context.save();
+	context.fillStyle = "rgba(0,0,0,0.5)";
+	context.rotate( headSegment.dir.angle(), 1/5 );
+	context.drawImage(
+		headSegment.img,
+		-headSegment.size,
+		-headSegment.width/2,
+		headSegment.size,
+		headSegment.width
+	);
+	context.restore();
+	
+	// -------------------------------------------------------------------------
+	context.restore();
+});
 
+//// Player Setup --------------------------------------------------------------
+
+// Create HTML Img elements
+GameAsset.player.headImg = new Image();
+GameAsset.player.headImg.src = "assets/graphics/wolf/wolf-head.png";
+GameAsset.player.frontImg = new Image();
+GameAsset.player.frontImg.src = "assets/graphics/wolf/wolf-front.png";
+GameAsset.player.behindImg = new Image();
+GameAsset.player.behindImg.src = "assets/graphics/wolf/wolf-behind.png";
+GameAsset.player.tailImg = new Image();
+GameAsset.player.tailImg.src = "assets/graphics/wolf/wolf-tail.png";
+
+// Things
+var segmentImages = [GameAsset.player.headImg, 
+				 GameAsset.player.frontImg,
+				 GameAsset.player.behindImg,
+				 GameAsset.player.tailImg];
+var segmentSizes = [0.6,0.6,0.6,0.8];
+var segmentWidths =[0.6,0.5,0.6,0.2];
+var segmentOffsets=[-0.1,-0.15,-0.1,0];
+var segmentSpring =[1/10,1/5,1/10,1/10];
+
+// Segments
+GameAsset.player.segments = [];
+var numSegments = 4;
+var positionSum = 0;
+var offsetSum = 0;
+for(var i=0; i < numSegments; i++){
+	GameAsset.player.segments.push({
+		pos: new Vector(positionSum, 0),// position (tiles)
+		dir: new Vector(1,0).norm(),	// vector in direction of segment
+		size: segmentSizes[i],
+		offset: segmentOffsets[i],
+		width: segmentWidths[i],
+		img: segmentImages[i],
+		spring: segmentSpring[i]
+	});
+	// accumulate sizes
+	positionSum += segmentSizes[i] + segmentOffsets[i];
+}
+GameAsset.player.vel = new Vector(0,0);
+
+////////////////////////////////////////////////////////////////////////////////
+
+/*GameAsset.player = new GameAsset(0.8, 0.8, true, function(x,y,asset,obj) {
+	// Save coordinate transforms to leave untouched by this function
+	context.save();
+	
+	// Move context origin to player's center
+	context.translate(x,y);
+	
+	// If the player is moving at least a little...
 	if (magnitude(obj.vx, obj.vy) > 0.03) {
-		// If the player is moving at least a little...
+		// turning in the direction they're moving
 		obj.drawx = obj.drawx * 0.9 + 0.1 * obj.vx;
 		obj.drawy = obj.drawy * 0.9 + 0.1 * obj.vy;
-		// turning in the direction they're moving
-		if (!isFinite(obj.drawx)) {
-			obj.drawx = 0;
-			obj.drawy = 0;
-			// Set drawx to 0 in case it became NaN
-		}
 	}
-
-	// We're centered on the player's center
-	// So rotate in the right direction
+	
+	// We're centered on the player's center so rotate in the right direction
 	context.rotate( Math.atan2(  obj.drawy, obj.drawx ) + Math.PI / 2 );
-
+	
 	// Move up a little to center on neck
 	context.translate(0, 0.44);
-
+	
+	// If we're currently leaping...
 	if (obj.leapwait > 45) {
-		// If we're currently leaping...
-
 		// Motion blur
 		for (var i = 0; i < 1.2; i += 0.4) {
 			context.globalAlpha = (1 - i);
@@ -45,14 +156,10 @@ GameAsset.player = new GameAsset(0.8, 0.8, true, function(x,y,asset,obj) {
 		}
 		context.globalAlpha = 1;
 	}
-
+	
 	// Draw the wolf
 	context.drawImage(asset.img,-.25,-.75, 0.5, 1.5);
-
+	
 	// Put the context like it was before
 	context.restore();
-});
-
-// Create HTML Img element:
-GameAsset.player.img = new Image();
-GameAsset.player.img.src = "assets/graphics/wolf_reference.png";
+});*/
