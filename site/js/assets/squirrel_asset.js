c47423ae696e16205194bda5bf46f99ab91ee832
Benjamin Bray <benrbray@umich.edu>
2014-10-14 01:24:05 -0400

    moved squirrel/human asset code

---
+++ site/js/assets/squirrel_asset.js
@@ -0,0 +1,35 @@
+/* SQUIRREL_ASSET.JS
+ */
+
+"use strict";
+
+////////////////////////////////////////////////////////////////////////////////
+
+GameAsset.squirrel = new GameAsset(0.8, 0.8, true, function(x,y,asset,obj) {
+	context.save();
+	// Save to leave untouched by this function
+	context.translate(x,y);
+	// Move context origin to player's center
+	if (magnitude(obj.vx, obj.vy) > 0.03) {
+		// If the player is moving at least a little...
+		obj.drawx = obj.drawx * 0.9 + 0.1 * obj.vx;
+		obj.drawy = obj.drawy * 0.9 + 0.1 * obj.vy;
+		// turning in the direction they're moving
+		if (!isFinite(obj.drawx)) {
+			obj.drawx = 0;
+			obj.drawy = 0;
+			// Set drawx to 0 in case it became NaN
+		}
+	}
+	// We're centered on the player's center
+	// So rotate in the right direction
+	context.rotate( Math.atan2(  obj.drawy, obj.drawx ) + Math.PI / 2 );
+	// Draw the wolf
+	context.drawImage(asset.img,-.4,-.4, 0.8, .8);
+	// Put the context like it was before
+	context.restore();
+});
+
+// Create HTML Img element:
+GameAsset.squirrel.img = new Image();
+GameAsset.squirrel.img.src = "assets/graphics/squirrel.png";
